024595dff5749fc3db30d501f6deca79d8c39f25
Josh Cooke <icecreamhead@gmail.com>
2023-12-02 14:57:20 +0000

    2023-q1 rewrite

--- twentytwentythree/src/main/java/com/icecreamhead/adventofcode/q1/Main.java
+++ twentytwentythree/src/main/java/com/icecreamhead/adventofcode/q1/Main.java
@@ -1,75 +1,69 @@
 package com.icecreamhead.adventofcode.q1;
 
 import com.icecreamhead.adventofcode.util.InputLoader;
+
 import java.util.List;
 import java.util.function.Function;
 
 public class Main {
+    public static void main(String[] args) {
+        List<String> calibrationLines = InputLoader.loadLines("q1/puzzle.txt");
 
-  /* Part 2 */
-  public static void main(String[] args) {
+        long total = calibrationLines.stream()
+//                .peek(line -> System.out.print(line + " => "))
+                .map(line -> firstDigit(line) + lastDigit(line))
+                .mapToLong(Long::parseLong)
+//                .peek(System.out::println)
+                .sum();
 
-    List<String> calibrationLines = InputLoader.loadLines("q1/puzzle.txt");
-
-    long total = calibrationLines.stream()
-        .peek(System.out::println)
-        .map(replaceNumberWords)
-        .peek(System.out::println)
-        .mapToLong(line -> {
-          String digits = line.replaceAll("[^\\d]", "");
-
-          return Long.parseLong(digits.charAt(0) + digits.substring(digits.length() - 1));
-        })
-        .peek(System.out::println)
-        .sum();
-
-    System.out.println(total);
-
-  }
-  /* Part 1 */
-  /*public static void main(String[] args) {
-    List<String> calibrationLines = InputLoader.loadLines("q1/puzzle.txt");
-
-    long total = calibrationLines.stream()
-        .mapToLong(line -> {
-          String digits = line.replaceAll("[^\\d]", "");
-
-          return Long.parseLong(digits.charAt(0) + digits.substring(digits.length() - 1));
-        })
-        .sum();
-
-    System.out.println(total);
-  }*/
-
-  private static final String[] NUMBERS = new String[]{
-      null, "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
-  };
-  private final static Function<String, String> replaceNumberWords = line -> {
-    int firstDigitIdx = Integer.MAX_VALUE, lastDigitIdx = Integer.MIN_VALUE;
-    int firstNumber = -1, lastNumber = -1;
-    int firstNumberIndex = Integer.MAX_VALUE, lastNumberIndex = Integer.MIN_VALUE;
-    for (int i = 1; i < 10; i++) {
-      int idx1, idx2;
-      if ((idx1 = line.indexOf(NUMBERS[i])) != -1 && idx1 < firstNumberIndex) {
-        firstNumberIndex = idx1;
-        firstNumber = i;
-      }
-      if ((idx2 = line.lastIndexOf(NUMBERS[i])) != -1 && idx2 > lastNumberIndex) {
-        lastNumberIndex = idx2;
-        lastNumber = i;
-      }
-      {
-        int idx;
-        if ((idx = line.indexOf(String.valueOf(i))) != -1 && idx < firstDigitIdx) firstDigitIdx = idx;
-      }
-      {
-        int idx;
-        if ((idx = line.lastIndexOf(String.valueOf(i))) != -1 && idx > lastDigitIdx) lastDigitIdx = idx;
-      }
+        System.out.println("\n" + total);
     }
 
-    if (firstNumber != -1 && firstNumberIndex < firstDigitIdx) line = line.replace(NUMBERS[firstNumber], String.valueOf(firstNumber));
-    if (lastNumber != -1 && lastNumberIndex > lastDigitIdx) line = line.replace(NUMBERS[lastNumber], String.valueOf(lastNumber));
-    return line;
-  };
+    private static String firstDigit(String line) {
+        char c;
+        return switch (c = line.charAt(0)) {
+            case '1', '2', '3', '4', '5', '6', '7', '8', '9' -> "%s".formatted(c);
+            case 'o', 't', 'f', 's', 'e', 'n' -> switch (line.substring(0, Math.min(5, line.length() - 1))) {
+                case "three" -> "3";
+                case "seven" -> "7";
+                case "eight" -> "8";
+                default -> switch (line.substring(0, Math.min(4, line.length() - 1))) {
+                    case "four" -> "4";
+                    case "five" -> "5";
+                    case "nine" -> "9";
+                    default -> switch (line.substring(0, Math.min(3, line.length() - 1))) {
+                        case "one" -> "1";
+                        case "two" -> "2";
+                        case "six" -> "6";
+                        default -> firstDigit(line.substring(1));
+                    };
+                };
+            };
+            default -> firstDigit(line.substring(1));
+        };
+    }
+
+    private static String lastDigit(String line) {
+        char c;
+        return switch (c = line.charAt(line.length() - 1)) {
+            case '1', '2', '3', '4', '5', '6', '7', '8', '9' -> "%s".formatted(c);
+            case 'e', 'o', 'x', 'r', 'n', 't' -> switch (line.substring(Math.max(0, line.length() - 5))) {
+                case "three" -> "3";
+                case "seven" -> "7";
+                case "eight" -> "8";
+                default -> switch (line.substring(Math.max(0, line.length() - 4))) {
+                    case "four" -> "4";
+                    case "five" -> "5";
+                    case "nine" -> "9";
+                    default -> switch (line.substring(Math.max(0, line.length() - 3))) {
+                        case "one" -> "1";
+                        case "two" -> "2";
+                        case "six" -> "6";
+                        default -> lastDigit(line.substring(0, line.length() - 1));
+                    };
+                };
+            };
+            default -> lastDigit(line.substring(0, line.length() - 1));
+        };
+    }
 }
