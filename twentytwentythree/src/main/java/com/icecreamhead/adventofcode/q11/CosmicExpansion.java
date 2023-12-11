6590bc82037bd59459bd7fab444b293449c1cbbc
Josh Cooke <josh.cooke@starlingbank.com>
2023-12-11 16:47:18 +0000

    2023-q11 wip

---
+++ twentytwentythree/src/main/java/com/icecreamhead/adventofcode/q11/CosmicExpansion.java
@@ -0,0 +1,147 @@
+package com.icecreamhead.adventofcode.q11;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class CosmicExpansion {
+
+  long part1(char[][] input) {
+//    printGrid(input);
+
+    for (int rowi = 0; rowi < input.length; rowi++) {
+      char[] row = input[rowi];
+      if (allEmpty(row)) {
+        input = duplicateRow(input, rowi, 1);
+        rowi++;
+      }
+    }
+//    printGrid(input);
+
+    for (int coli = 0; coli < input[0].length; coli++) {
+      boolean allBlank = true;
+      for (int rowi = 0; rowi < input.length; rowi++) {
+        if (input[rowi][coli] != '.') {
+          allBlank = false;
+          break;
+        }
+      }
+      if (allBlank) {
+        input = duplicateCol(input, coli, 1);
+        coli++;
+      }
+    }
+//    printGrid(input);
+
+    return calculateDistances(input);
+  }
+
+  long part2(char[][] input, int expansion) {
+//    printGrid(input);
+
+    for (int rowi = 0; rowi < input.length; rowi++) {
+      char[] row = input[rowi];
+      if (allEmpty(row)) {
+        input = duplicateRow(input, rowi, expansion - 1);
+        rowi++;
+      }
+    }
+//    printGrid(input);
+
+    for (int coli = 0; coli < input[0].length; coli++) {
+      boolean allBlank = true;
+      for (int rowi = 0; rowi < input.length; rowi++) {
+        if (input[rowi][coli] != '.') {
+          allBlank = false;
+          break;
+        }
+      }
+      if (allBlank) {
+        input = duplicateCol(input, coli, expansion - 1);
+        coli++;
+      }
+    }
+//    printGrid(input);
+
+    return calculateDistances(input);
+  }
+
+  private long calculateDistances(char[][] input) {
+    List<Coord> galaxies = new ArrayList<>();
+
+    for (int row = 0; row < input.length; row++) {
+      for (int col = 0; col < input[row].length; col++) {
+        if (input[row][col] == '#') galaxies.add(new Coord(col, row));
+      }
+    }
+
+    long total = 0;
+    for (Coord galaxy1 : galaxies) {
+      for (Coord galaxy2 : galaxies) {
+        if (!galaxy1.equals(galaxy2)) total += (Math.abs(galaxy1.x - galaxy2.x) + Math.abs(galaxy1.y - galaxy2.y));
+      }
+    }
+
+    return total / 2;
+  }
+
+  private static boolean allEmpty(char[] row) {
+    for (char c : row) {
+      if (c != '.') {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private static char[][] duplicateRow(char[][] input, int idx, int times) {
+    System.out.printf("Input: %d rows, %d cols%n", input.length, input[0].length);
+    char[][] out = new char[input.length + times][input[0].length];
+    int offset = 0;
+    for (int row = 0; row < input.length; row++) {
+      System.out.printf("in.length=%d row=%d offset=%d idx=%d, out.length=%d%n", input.length, row, offset, (row+offset), out.length);
+      out[row + offset] = input[row];
+      if (row == idx) {
+        for (int i = 1; i <= times; i++) {
+          System.out.printf("Copying row %d to %d%n", row, row+offset+i);
+          out[row + offset + i] = input[row];
+        }
+        offset++;
+      }
+    }
+    System.out.printf("Output: %d rows, %d cols%n", out.length, out[0].length);
+    return out;
+  }
+
+  private static char[][] duplicateCol(char[][] input, int idx, int times) {
+//    System.out.printf("Input: %d rows, %d cols%n", input.length, input[0].length);
+    char[][] out = new char[input.length][input[0].length + times];
+    for (int row = 0; row < input.length; row++) {
+//      System.out.printf("in.length=%d row=%d offset=%d idx=%d, out.length=%d%n", input.length, row, offset, (row+offset), out.length);
+      int offset = 0;
+      for (int col = 0; col < input[0].length; col++) {
+        out[row][col + offset] = input[row][col];
+        if (col == idx) {
+          System.out.printf("Copying col %d to %d%n", row, row+offset);
+          for (int i = 1; i <= times; i++) {
+            out[row][col + offset + i] = input[row][col];
+          }
+          offset++;
+        }
+      }
+    }
+//    System.out.printf("Output: %d rows, %d cols%n", out.length, out[0].length);
+    return out;
+  }
+
+  private static void printGrid(char[][] grid) {
+    for (int y = 0; y < grid.length; y++) {
+      System.out.println(y + ":\t" + Arrays.toString(grid[y]));
+    }
+    System.out.println();
+  }
+
+  record Coord(int x, int y){}
+}
---
+++ twentytwentythree/src/test/java/com/icecreamhead/adventofcode/q11/CosmicExpansionTest.java
@@ -0,0 +1,28 @@
+package com.icecreamhead.adventofcode.q11;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.icecreamhead.adventofcode.util.InputLoader;
+import org.junit.jupiter.api.Test;
+
+class CosmicExpansionTest {
+
+  private final CosmicExpansion underTest = new CosmicExpansion();
+
+  private static final char[][] SAMPLE = InputLoader.loadGrid("q11/sample.txt");
+  private static final char[][] PUZZLE = InputLoader.loadGrid("q11/puzzle.txt");
+
+  @Test
+  void part1_sample() {
+    assertThat(underTest.part1(SAMPLE)).isEqualTo(374);
+  }
+  @Test
+  void part1_puzzle() {
+    System.out.println(underTest.part1(PUZZLE));
+  }
+  @Test
+  void part2_sample() {
+    assertThat(underTest.part2(SAMPLE, 10)).isEqualTo(1030);
+    assertThat(underTest.part2(SAMPLE, 100)).isEqualTo(8410);
+  }
+}
